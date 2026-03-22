0493efd54df0177f0e8d98fb8d9ec467bbecee95
Ulrike Kapeller <ukapeller.mmt-b2023@fh-salzburg.ac.at>
2026-03-22 14:27:26 +0100

    added the stuff for menu start, exit button to work and in the main the back to scene one

--- Assets/Scripts/MenuUIHandler.cs
+++ Assets/Scripts/MenuUIHandler.cs
@@ -1,14 +1,20 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.SceneManagement; 
 
 // Sets the script to be executed later than all default scripts
 // This is helpful for UI, since other things may need to be initialized before setting the UI
 [DefaultExecutionOrder(1000)]
 public class MenuUIHandler : MonoBehaviour
 {
-    public ColorPicker ColorPicker;
+     public ColorPicker ColorPicker;
+     public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    } 
 
     public void NewColorSelected(Color color)
     {
@@ -21,4 +27,17 @@ public class MenuUIHandler : MonoBehaviour
         //this will call the NewColorSelected function when the color picker have a color button clicked.
         ColorPicker.onColorChanged += NewColorSelected;
     }
+
+    public void ExitGame()
+    {
+        // this will quit the application when the exit button is clicked
+        Application.Quit();
+
+        // this line stops the play mode in the editor when the exit button is clicked
+        #if UNITY_EDITOR
+        EditorApplication.isPlaying = false;
+        #endif
+    }
+   
+
 }
